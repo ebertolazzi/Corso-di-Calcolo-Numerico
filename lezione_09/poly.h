c75394b395f9c17019ae1584056fe9a4fc6ceda9
Alice Plebe <alice.plebe@gmail.com>
2021-11-14 15:22:21 +0100

    Indentation fixed
    Tabs changed to spaces

--- lezione_09/poly.h
+++ lezione_09/poly.h
@@ -1,7 +1,7 @@
 /* --------------------------------------------------------------------------------- *\
-	CALCOLO NUMERICO E PROGRAMMAZIONE C++ (AA 2021/22)                  Alice Plebe
+    CALCOLO NUMERICO E PROGRAMMAZIONE C++ (AA 2021/22)                  Alice Plebe
 
-	Classe per rappresentare un poligono
+    Classe per rappresentare un poligono
 \* --------------------------------------------------------------------------------- */
 
 #pragma once
@@ -9,26 +9,26 @@
 #include <Eigen/Dense>
 
 class Poly : public Eigen::VectorXd {
-	private:
-		Eigen::VectorXd     v;              // vettore dei coefficienti
-		int		            deg;            // grado del polinomio
-											// (o meglio, la lunghezza del vettore v)
+    private:
+        Eigen::VectorXd     v;              // vettore dei coefficienti
+        int		            deg;            // grado del polinomio
+                                            // (o meglio, la lunghezza del vettore v)
 
-	public:
-		// costruttori
-		Poly( int );
-		Poly( const Eigen::VectorXd );
+    public:
+        // costruttori
+        Poly( int );
+        Poly( const Eigen::VectorXd );
 
-		// funzioni di supporto
-		Eigen::VectorXd get_coef() { return v; }
-		int get_deg() { return deg-1; }
+        // funzioni di supporto
+        Eigen::VectorXd get_coef() { return v; }
+        int get_deg() { return deg-1; }
 
-		// stampa il polinomio
-		friend std::ostream &operator<<( std::ostream&, const Poly& );
+        // stampa il polinomio
+        friend std::ostream &operator<<( std::ostream&, const Poly& );
 
-		double eval( float );               // valuta il polinomio
-		Poly operator+( const Poly& );      // operazione somma
-		Poly operator*( const Poly& );      // operazione prodotto
-		Poly derivative();                  // operazione derivata
-		Poly integral();                    // operazione integrale
+        double eval( float );               // valuta il polinomio
+        Poly operator+( const Poly& );      // operazione somma
+        Poly operator*( const Poly& );      // operazione prodotto
+        Poly derivative();                  // operazione derivata
+        Poly integral();                    // operazione integrale
 };
